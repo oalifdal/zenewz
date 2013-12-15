8aef3e493578b1fe6d001cb76ad19663a4285e2f
Omar Ali Fdal <omar.alifdal@gmail.com>
2013-12-15 15:48:08 +0100

    Add extension files

---
+++ zen.js
@@ -0,0 +1,26 @@
+// 2013 
+// Author: Omar Ali Fdal
+
+
+function zenRead() {
+  // then injectCSS;
+  //hide #offre_decouverte_form 
+  //hid div fond_overlay offre_decouverte
+  console.log("Injecting css");
+  var link = document.createElement("link");
+  link.href = chrome.extension.getURL("zen.css");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+
+setTimeout(function() {
+  var zenButton = document.querySelector("div.btn_lecture_zen");
+  //if div.btn_lecture_zen exists click on it
+  if (zenButton !== null) {
+    console.log("Found the zen button");
+    zenButton.onclick = zenRead ;
+    zenButton.click();
+  }
+}, 3000);
